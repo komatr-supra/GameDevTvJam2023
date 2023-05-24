8ad443223ed3bdb6879ac4d2174bd12215bf2ef9
Marcus Sanatan <msanatan@gmail.com>
2023-05-24 01:00:34 -0400

    Move scriptable objects folder + make actions available at start

--- Assets/Scripts/AttackConfiguration.cs
+++ Assets/Scripts/AttackConfiguration.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Attack", menuName = "Character Attack", order = 1)]
 public class AttackConfiguration : ScriptableObject
 {
+    public bool isAvailable = true;
     public bool isRanged = false;
     public int damage = 1;
     public float attackRadius = 1f;
